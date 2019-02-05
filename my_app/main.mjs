05d12c44c639d0514f066bc01b8874091f908915
t-kisimoto <kuroneko59@icloud.com>
2019-02-05 12:01:02 +0900

    add_w_log

--- my_app/main.mjs
+++ my_app/main.mjs
@@ -31,7 +31,10 @@ async function requestGeoCoderAPI(query) {
       output: 'json',
     },
   });
-  console.log(response);
+  const w_response = await axios({
+    url: "http://weather.livedoor.com/forecast/webservice/json/v1?city=011000"
+  });
+  console.log(w_response);
   const { data } = response;
   // APIから受け取った内容をコンソールに表示
   console.log(JSON.stringify(data, undefined, 2));
